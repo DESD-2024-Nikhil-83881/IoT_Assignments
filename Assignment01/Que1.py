00024b9184d9901eca05c02092479c9220f34745
NikhilBorate <nikhilborate1998@gmail.com>
2024-04-18 15:03:55 +0530

    assignment

---
+++ Assignment01/Que1.py
@@ -0,0 +1,9 @@
+le = float(input("Enter length of rectangle: "))
+
+br = float(input("Enter length of rectangle: "))
+
+area = le * br
+print(f"area of rectangle = {area}")
+
+perimeter = 2*le + 2*br
+print(f"perimeter of rectangle = {perimeter}")
---
+++ Assignment01/Que3.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
+
+sum = num1 + num2 + num3
+
+avg = sum/3
+
+print(f"average  = {avg}")
